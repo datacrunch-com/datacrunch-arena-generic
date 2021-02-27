5ebcc2d696b22f9c31176ae566e3a110b67cb461
Caceresenzo <caceresenzo1502@gmail.com>
2021-02-27 20:25:13 +0100

    doc: Added docstring for helper.load_model

--- helper.py
+++ helper.py
@@ -4,6 +4,23 @@ import joblib
 
 
 def load_model(path):
+    """
+    Load a model with joblib, but check if the file exists first.
+    This is to print a more friendly message.
+    
+    Parameters
+    ----------
+    path : str
+        File path to check.
+    
+    Returns
+    -------
+    joblib.load(path)
+    
+    Notes
+    -----
+    Aside from check if the file is readable, nothing else is done.
+    """
     if not os.access(path, os.R_OK):
         print(f"argh... the file for the model {path} does not exists or is not readable, are you sure you put it in?")
         exit(1)
